673bfc293e84a15b54e176f409c77168f6cbbf89
Firmanmu <firmanmuhaemin100@gmail.com>
2024-08-21 08:51:47 +0700

    Mengubah Navbar menjadi default lagi

--- crudperpustakaan-app/resources/views/library.blade.php
+++ crudperpustakaan-app/resources/views/library.blade.php
@@ -16,15 +16,9 @@
 {{-- endNotif --}}
 
     {{-- navbar --}}
-  <nav class="navbar bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand">Daftar Buku-Buku</a>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+    <div class="container">
+      <h1 class="text-center">Daftar Buku-Buku</h1>
     </div>
-  </nav>
   {{-- EndNavbar --}}
 
     {{-- table --}}
